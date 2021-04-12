2c3ed8b8eb18a2b2c5b6511ae7b2e07007b14e2d
huangri212 <huangri212@163.com>
2021-04-13 01:12:53 +0800

    Update ProductController.java

--- src/main/java/com/ensat/controllers/ProductController.java
+++ src/main/java/com/ensat/controllers/ProductController.java
@@ -63,7 +63,6 @@ public class ProductController {
     @RequestMapping("product/new")
     public String newProduct(Model model) {
         model.addAttribute("product", new Product());
-        System.out.println("create Successfully");
         return "productform";
     }
 
